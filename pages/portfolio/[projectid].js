b01b19dab33b96f823e2540403abb2e596bb3a9d
Daniel <dunapanta2@gmail.com>
2021-04-02 19:34:40 -0500

    Extracing dynamic path segment data

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,12 @@
+import { useRouter } from 'next/router'
+
 const ProtfolioProjectPage = () => {
+    const router = useRouter()
+
+    console.log(router.pathname) //[portfolio/[projectid]
+    console.log(router.query)   // /{projectid: "1"}
+
+
     return (
         <div>
             <h1>Portfolio Poject Page</h1>
